338ee378561f0f3d7805d572b7e64a7ec03836e8
katerina <katerina55882@gmail.com>
2023-12-21 01:01:31 +0300

    feat: add Lab4

---
+++ Lab4/TodoList/Models/Task.cs
@@ -0,0 +1,25 @@
+namespace TodoList.Models;
+
+public class Task
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; }
+    public DateTime Deadline { get; }
+    public DateTime LastModifiedDateTime { get; }
+    public List<string> Tags { get; }
+
+    public Task()
+    {
+    }
+
+    public Task(Guid id, string title, string description, DateTime deadline, DateTime lastModifiedDateTime, List<string> tags)
+    {
+        Id = id;
+        Title = title;
+        Description = description;
+        Deadline = deadline;
+        LastModifiedDateTime = lastModifiedDateTime;
+        Tags = tags;
+    }
+}
